0099729a94f6acbb10700fbe2e2ec73cf6c9b446
pyontarou <kooooo7150@gmail.com>
2021-02-15 18:07:03 +0900

    実装要件のバリデーションの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :password , format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message:'英字と数字の両方を含めて設定してください' }      
 
-  validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_day, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birth_day
+
+    with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください'} do
+      validates :family_name
+      validates :first_name
+    end
+
+    with_options format: {with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください'} do
+      validates :family_name_kana 
+      validates :first_name_kana
+    end
+
+  end
 end
