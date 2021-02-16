fb22781189920795b61316ec269994e9223c8bb6
pyontarou <kooooo7150@gmail.com>
2021-02-16 10:55:16 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,14 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :order
   has_one_attached :image
-  
+
+  validates :name, presence:true
+  validates :description, presence:true
+  validates :category_id, presence:true
+  validates :state_id, presence:true
+  validates :postage_id, presence:true
+  validates :prefectures_idame, presence:true
+  validates :shipping_date_id, presence:true
+  validates :price, presence:true
+
 end
