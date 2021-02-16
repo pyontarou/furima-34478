cbd5331655ff0adb3a8290e0e2206ebe0a6fb294
pyontarou <kooooo7150@gmail.com>
2021-02-16 11:32:44 +0900

    ActiveHashのデータの定義、アソシエーション、バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,14 +2,19 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :order
   has_one_attached :image
+  belongs_to :category
+  belongs_to :state
+  belongs_to :postage
+  belongs_to :prefectures
+  belongs_to :shipping_date
 
   validates :name, presence:true
   validates :description, presence:true
-  validates :category_id, presence:true
-  validates :state_id, presence:true
-  validates :postage_id, presence:true
-  validates :prefectures_idame, presence:true
-  validates :shipping_date_id, presence:true
-  validates :price, presence:true
+  validates :category_id,  numericality: { other_than: 1 }
+  validates :state_id,  numericality: { other_than: 1 }
+  validates :postage_id,  numericality: { other_than: 1 }
+  validates :prefectures_idame,  numericality: { other_than: 1 }
+  validates :shipping_date_id,  numericality: { other_than: 1 }
+  validates :price,  numericality: { other_than: 1 }
 
 end
