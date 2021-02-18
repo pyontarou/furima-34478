335f8f830f5e257846559830dc425bb8a96e73cd
pyontarou <kooooo7150@gmail.com>
2021-02-18 22:23:20 +0900

    Formオブジェクトパターンでの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,26 @@
 class OrdersController < ApplicationController
   def index
-    
+    @order = Order.new
+    @item = Item.find(params[:item_id]) 
+    @order_address = OrderAddress.new
   end
+
+  def create
+    @item = Item.find(params[:item_id]) 
+    @order_address  = OrderAddress.new(order_params)
+    if @order_address.valid?
+       @order_address.save
+       redirect_to root_path
+    else
+       render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:prefecture_code, :prefectures_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id).merge(user_id: current_user.id,item_id: params[:item_id])
+  end
+ 
+
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,17 +1,9 @@
 class Address < ApplicationRecord
   belongs_to :order
 
-  with_options presence: true do
-    validates :city
-    validates :house_number
-    validates :building_name
-    validates :prefectures_id, numericality: { other_than: 1 }
-    validates :prefecture_code,format: { with: /\A\d{3}[-]\d{4}\z/, message: '郵便番号はハイフンが必要です'}
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: '電話番号は11桁以内の数値を入力してください' }
-  end
-
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefectures
+
 
 end
 
