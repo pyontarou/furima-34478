dac16c04f009eac8dd71e6cf04f0ea009f9da507
pyontarou <kooooo7150@gmail.com>
2021-02-19 14:54:07 +0900

    クレジットカード決済での購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,26 +1,36 @@
 class OrdersController < ApplicationController
   def index
-    @order = Order.new
     @item = Item.find(params[:item_id]) 
     @order_address = OrderAddress.new
   end
 
   def create
     @item = Item.find(params[:item_id]) 
-    @order_address  = OrderAddress.new(order_params)
+    @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-       @order_address.save
-       redirect_to root_path
+      pay_item
+      @order_address.save
+      redirect_to root_path
     else
-       render :index
+      render :index
     end
   end
-
+  
   private
-
+  
   def order_params
-    params.require(:order_address).permit(:prefecture_code, :prefectures_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id).merge(user_id: current_user.id,item_id: params[:item_id])
+    params.require(:order_address).permit(:prefecture_code, :prefectures_id, :city, :house_number, :building_name, :phone_number).merge(token: params[:token],user_id: current_user.id,item_id: params[:item_id])
   end
+  
+  def pay_item
+    Payjp.api_key = "sk_test_b0e6ae1a24fd570edec47e1e"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+  
  
 
 end
