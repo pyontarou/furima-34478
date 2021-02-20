858d4d9a96569158d5a35a3b0aa20bd401613257
pyontarou <kooooo7150@gmail.com>
2021-02-20 14:47:45 +0900

    トークンの環境変数読込み修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,7 +25,7 @@ class OrdersController < ApplicationController
   end
   
   def pay_item
-    Payjp.api_key = "sk_test_b0e6ae1a24fd570edec47e1e"
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,
       card: order_params[:token],
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_fadbe5b8d1b0a48ec61d5034"); // PAY.JPテスト公開鍵
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY); // PAY.JPテスト公開鍵
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
