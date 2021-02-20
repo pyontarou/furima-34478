89b4f9762f012801d88824b05b746cfabd811ce0
pyontarou <kooooo7150@gmail.com>
2021-02-20 12:18:34 +0900

    バリデーションの追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -8,6 +8,8 @@ class OrderAddress
     validates :city
     validates :house_number
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'Input only number' }
+    validates :user_id
+    validates :item_id
   end
 
   
