3220ca4956f97fc2aba5d6d55fa01aa0b5eee038
pyontarou <kooooo7150@gmail.com>
2021-02-22 15:54:20 +0900

    単体テストコード

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Message do
+  before do
+    @message = FactoryBot.build(:message)
+  end
+    
+  describe 'メッセージ機能' do
+    context 'メッセージが保存できるとき' do
+      it 'textが存在していれば保存できること' do
+        expect(@message).to be_valid
+      end
+    end
+
+    context 'メッセージが保存できないとき' do
+      it 'textが空では保存できないこと' do
+        @message.text = ""
+        @message.valid?
+        expect(@message.errors.full_messages).to include("Text can't be blank" )
+      end
+
+      it 'userが紐づいていないと保存できないこと' do
+        @message.user = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("User must exist")
+      end
+      
+      it 'itemが紐づいていないと保存できないこと' do
+        @message.item = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("Item must exist")
+        
+      end
+    end
+  end
 end
